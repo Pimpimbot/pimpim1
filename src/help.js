152c3e66aeec37553f9a5a344af18501c29ba95a
Pimpimbot <86252942+Pimpimbot@users.noreply.github.com>
2021-06-21 13:37:46 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,88 @@
+const help = (prefix) => {
+	return `
+「 *Reb BOT* 」
+
+◪ *informações*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Criador : *PiMPiM*
+  ⚠️ alguns comandos não funciona ⚠️
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *FAZER*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  ├─ ❏ ${prefix}tomp3
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}marvellogo
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}ninjalogo
+  ├─ ❏ ${prefix}water
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}logowolf
+  ├─ ❏ ${prefix}logowolf2
+  ├─ ❏ ${prefix}phlogo
+  ├─ ❏ ${prefix}glitch
+  ├─ ❏ ${prefix}neonlogo
+  ├─ ❏ ${prefix}neonlogo2
+  ├─ ❏ ${prefix}lionlogo
+  ├─ ❏ ${prefix}jokerlogo
+  ├─ ❏ ${prefix}shadow
+  ├─ ❏ ${prefix}burnpaper
+  ├─ ❏ ${prefix}coffee
+  ├─ ❏ ${prefix}lovepaper
+  ├─ ❏ ${prefix}woodblock
+  ├─ ❏ ${prefix}qowheart
+  ├─ ❏ ${prefix}mutgrass
+  ├─ ❏ ${prefix}undergocean
+  ├─ ❏ ${prefix}woodenboards
+  ├─ ❏ ${prefix}wolfmetal
+  ├─ ❏ ${prefix}metalictglow
+  ├─ ❏ ${prefix}8bit
+  ├─ ❏ ${prefix}ttp
+  ├─ ❏ ${prefix}herrypotter
+  ├─ ❏ ${prefix}pubglogo
+  └─ ❏ ${prefix}quotemaker
+◪ *DOWNLOADER*
+  │
+  ├─ ❏ ${prefix}images
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  ├─ ❏ ${prefix}tiktok
+  └─ ❏ ${prefix}joox
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo
+◪ *SOM*
+  │
+  ├─ ❏ ${prefix}play
+  └─ ❏ ${prefix}tts
+◪ *MÚSICA*
+  │
+  ├─ ❏ ${prefix}lirik
+  └─ ❏ ${prefix}chord
+◪ *WIBU*
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
+  ├─ ❏ ${prefix}randomanime
+  ├─ ❏ ${prefix}husbu
+  ├─ ❏ ${prefix}husbu2
+  ├─ ❏ ${prefix}wait
+  └─ ❏ ${prefix}nekonime
+
+`
+}
+
+exports.help = help
